c1fcd9e16e914b2bfaff1fd59b4b0fd261e99129
PRINCE ARIYAN <113643112+Itz-Ariyan@users.noreply.github.com>
2022-12-20 21:22:59 +0600

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ load_dotenv()
 API_ID = getenv("API_ID", "23842900")
 API_HASH = getenv("API_HASH", "d21e95895cf2a5b83b0167fdd3b6e541")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "mongodb+srv://Xx:Xx@cluster0.mwyxrf1.mongodb.net/?retryWrites=true&w=majority")
+BOT_TOKEN = getenv("BOT_TOKEN", "5915949693:AAGkojAqOwW5W7RFnJ2OzRDu5c4LqjtpRII")
 LOGGER_ID = int(getenv("LOGGER_ID", "-1001126273421"))
 OWNER_ID = int(getenv("OWNER_ID", "5761513990"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://dur:dur@cluster0.0o0qovt.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Xx:Xx@cluster0.mwyxrf1.mongodb.net/?retryWrites=true&w=majority")
 MUST_JOIN = getenv("MUST_JOIN", "ariyan_server")
