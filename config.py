79806df8cfc6e17dac767100726c49f025080619
PRINCE ARIYAN <113643112+Nixa-XD@users.noreply.github.com>
2023-01-01 02:57:08 +0600

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = getenv("API_ID", "23842900")
 API_HASH = getenv("API_HASH", "d21e95895cf2a5b83b0167fdd3b6e541")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "5708696355:AAGnyWx4wTSq0iaCo8WcfCaBZHM6B1apAVE")
+BOT_TOKEN = getenv("BOT_TOKEN", "5915949693:AAGydCgq2ymdL5RbwQg25OsLDkE5WxbbjAA")
 LOGGER_ID = int(getenv("LOGGER_ID", "-1001632995162"))
 OWNER_ID = int(getenv("OWNER_ID", "5761513990"))
 
