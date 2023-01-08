1bd1cabfe01879f36e53ee55f3dfb26900ef0f39
SakibR20 <122209718+SakibR20@users.noreply.github.com>
2023-01-09 02:24:28 +0600

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 API_ID = getenv("API_ID", "23842900")
 API_HASH = getenv("API_HASH", "d21e95895cf2a5b83b0167fdd3b6e541")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "5684117084:AAEYvEI2y6XP8Vq5tBWZT7CxpF1QCYODluM")
-LOGGER_ID = int(getenv("LOGGER_ID", "-1001632995162"))
+BOT_TOKEN = getenv("BOT_TOKEN", "5915949693:AAENyrtOP91JxS7txjD9yUpB3bCKK8AqPos")
+LOGGER_ID = int(getenv("LOGGER_ID", "-1001126273421"))
 OWNER_ID = int(getenv("OWNER_ID", "5761513990"))
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Xx:Xx@cluster0.mwyxrf1.mongodb.net/?retryWrites=true&w=majority")
